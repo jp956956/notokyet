6cedd85fc41212db6f0fd4556bb3152515af5b32
jp956956 <38661378+jp956956@users.noreply.github.com>
2021-02-02 07:48:34 +0000

    Deploying to gh-pages from master 574551f2611e7119049099910d63259108f2aa4f

---
+++ precache-manifest.d309809e669a113c6b6cca201c11e91b.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bacb6acd08ad2c6fa0b",
+    "url": "css/app.e7a3692c.css"
+  },
+  {
+    "revision": "7bbb343a640307d3903c",
+    "url": "css/chunk-vendors.8c5ffde0.css"
+  },
+  {
+    "revision": "33fb7f4fee2b29746fc1",
+    "url": "css/game.4d2c1ae1.css"
+  },
+  {
+    "revision": "93725b021a881785692a",
+    "url": "css/live.4d2c1ae1.css"
+  },
+  {
+    "revision": "ada842afc339d916c8fe",
+    "url": "css/perform.eb7ed01d.css"
+  },
+  {
+    "revision": "21cec40b8bd50b48cefd",
+    "url": "css/shop.2e646c97.css"
+  },
+  {
+    "revision": "8fcdfe041d1d082fd97219da0766790b",
+    "url": "img/bg2.8fcdfe04.jpg"
+  },
+  {
+    "revision": "1b30de2b7bcc06bf012bc62880142f6f",
+    "url": "img/logo.1b30de2b.svg"
+  },
+  {
+    "revision": "811766d5364c84f9a6a1221d6c9fbc79",
+    "url": "img/兄弟.811766d5.png"
+  },
+  {
+    "revision": "e5546041916f47426e75635d56370184",
+    "url": "img/唐肇廷.e5546041.jpg"
+  },
+  {
+    "revision": "5a9365186ed47df1320afda10c2d3cb2",
+    "url": "img/富邦.5a936518.jpg"
+  },
+  {
+    "revision": "270d1373d31e8132c7399cf3162fe3e5",
+    "url": "img/林安可.270d1373.jpg"
+  },
+  {
+    "revision": "83b0e9bdaec8b9bf07a8626b3ab13f29",
+    "url": "img/林安可.83b0e9bd.jpg"
+  },
+  {
+    "revision": "7782e694115e48a6abb32386fc313255",
+    "url": "img/棒球記分板.7782e694.png"
+  },
+  {
+    "revision": "50251b9f83bc7a6b4ab4b79f61e34d7a",
+    "url": "img/樂天.50251b9f.jpg"
+  },
+  {
+    "revision": "f1a395b9e3f626d17e56817b1d098271",
+    "url": "img/潘傑楷.f1a395b9.jpg"
+  },
+  {
+    "revision": "c774ac922ff96fd4cf16f3f423d842d7",
+    "url": "img/米蘭達.c774ac92.jpg"
+  },
+  {
+    "revision": "abbcbf08531b65bb832a890eb52b8475",
+    "url": "img/統一.abbcbf08.jpg"
+  },
+  {
+    "revision": "59e3a08776d6860e21a2e7a9bd0c051c",
+    "url": "img/陳傑憲.59e3a087.jpg"
+  },
+  {
+    "revision": "ff581a5a43dce16420fa0110ec7e6444",
+    "url": "img/陳韻文.ff581a5a.jpg"
+  },
+  {
+    "revision": "a6ef1d855ad72b5598a7383accd82e41",
+    "url": "index.html"
+  },
+  {
+    "revision": "1bacb6acd08ad2c6fa0b",
+    "url": "js/app.eed8075a.js"
+  },
+  {
+    "revision": "7bbb343a640307d3903c",
+    "url": "js/chunk-vendors.6ca7e8b4.js"
+  },
+  {
+    "revision": "517ef74ec7b21646021e",
+    "url": "js/connection.6e86d9c4.js"
+  },
+  {
+    "revision": "33fb7f4fee2b29746fc1",
+    "url": "js/game.b3b881b8.js"
+  },
+  {
+    "revision": "07cce565ab9aefda55ec",
+    "url": "js/introduce.5e8b10c2.js"
+  },
+  {
+    "revision": "93725b021a881785692a",
+    "url": "js/live.1ff8c33b.js"
+  },
+  {
+    "revision": "ada842afc339d916c8fe",
+    "url": "js/perform.23344c79.js"
+  },
+  {
+    "revision": "21cec40b8bd50b48cefd",
+    "url": "js/shop.ed920690.js"
+  },
+  {
+    "revision": "7fc0032efb6c4dc38389",
+    "url": "js/teamrecord.84279bb2.js"
+  },
+  {
+    "revision": "adda5eaec07208ca772807573e61bc9b",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
